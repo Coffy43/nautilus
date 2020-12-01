9fb0669eac213819811b7b77b3de5227bba64faf
Coffy43 <Brandon@Brandons-iMac.local>
2020-11-30 22:43:11 -0500

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { StaffComponent } from './pages/staff/staff.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'staff', component: StaffComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { StaffComponent } from './pages/staff/staff.component';
+import { MatCardModule } from '@angular/material/card';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAtAleMHfq--PtoKRxCFlwuMlnYTvjsW0",
+  authDomain: "nautilus-4207a.firebaseapp.com",
+  databaseURL: "https://nautilus-4207a.firebaseio.com",
+  projectId: "nautilus-4207a",
+  storageBucket: "nautilus-4207a.appspot.com",
+  messagingSenderId: "246723781840",
+  appId: "1:246723781840:web:ab34f1fe30b5ed0ac0ead5",
+  measurementId: "G-0Z6WW8B1L5"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TopnavComponent,
+    StaffComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, 
+    AngularFireAuthModule, 
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
